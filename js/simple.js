d15bb4c1493733ac3df0038a387d6d0a6ca9f1e7
Prasanna Shiwakoti <metal1kmonk3y@gmail.com>
2016-07-20 23:48:25 +0000

    flex menu with body spacing

--- js/simple.js
+++ js/simple.js
@@ -4,6 +4,20 @@ var main = function() {
   //except for nav links
   $("a.nav-link").removeAttr('target');
   $("#backToTop").removeAttr('target');
+  
+   //when window is resized change body padding
+  $(window).resize(function() {
+    var width = $(window).width(); 
+    $("body").append(width);
+    if(width > 750 ){
+      $("body").css("padding-top", 100); 
+      window.location.reload();
+    }
+    else {
+      $("body").css("padding-top", 400); 
+      window.location.reload();
+    }
+  });
 };
 
 $(document).ready(main);
