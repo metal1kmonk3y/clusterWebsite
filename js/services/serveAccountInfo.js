c46a831b78e3b71584833bf9badf940835ba2943
Prasanna Shiwakoti <metal1kmonk3y@gmail.com>
2016-08-17 08:45:52 +0000

    services linked to server

--- js/services/serveAccountInfo.js
+++ js/services/serveAccountInfo.js
@@ -1,6 +1,6 @@
 /* global app */
-app.factory('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/accountInfo.json', ['$http', function($http) { 
-  return $http.get('json/account_info.json') 
+app.factory('serveAccountInfo', ['$http', function($http) { 
+  return $http.get('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/accountInfo.json') 
             .success(function(data) { 
               return data; 
             })
--- js/services/serveArchitecture.js
+++ js/services/serveArchitecture.js
@@ -1,6 +1,6 @@
 /* global app */
-app.factory('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/architecture.json', ['$http', function($http) { 
-  return $http.get('json/architecture.json') 
+app.factory('serveArchitecture', ['$http', function($http) { 
+  return $http.get('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/architecture.json') 
             .success(function(data) { 
               return data; 
             })
