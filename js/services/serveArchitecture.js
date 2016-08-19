dad9aaac574ee82c0991981e041bab0ba88e6636
Prasanna Shiwakoti <metal1kmonk3y@gmail.com>
2016-08-19 20:46:25 +0000

    architecture file fixed

--- js/services/serveArchitecture.js
+++ js/services/serveArchitecture.js
@@ -1,6 +1,6 @@
 /* global app */
 app.factory('serveArchitecture', ['$http', function($http) { 
-  return $http.get('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/architecturere.json') 
+  return $http.get('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/architecture.json') 
             .success(function(data) { 
               return data; 
             })
