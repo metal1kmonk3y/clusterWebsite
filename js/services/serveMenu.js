2468c96fe4753b22688fbdf31fdb0d38fe3b2621
Prasanna <metal1kmonk3y@gmail.com>
2017-07-02 11:50:25 -0500

    Update serveMenu.js

--- js/services/serveMenu.js
+++ js/services/serveMenu.js
@@ -1,6 +1,6 @@
 /* global app */
 app.factory('serveMenu', ['$http', function($http) { 
-  return $http.get('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/menuItems.json') 
+  return $http.get('http://13.59.248.205/menuItems.json') 
             .success(function(data) { 
               return data; 
             })
