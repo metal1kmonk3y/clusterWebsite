b8e9cf8649f94d3299d85a7c9cd968863ec641fa
Prasanna <metal1kmonk3y@gmail.com>
2017-07-02 11:49:56 -0500

    Update serveFooter.js

--- js/services/serveFooter.js
+++ js/services/serveFooter.js
@@ -1,6 +1,6 @@
 /* global app */
 app.factory('serveFooter', ['$http', function($http) { 
-  return $http.get('http://flux.cs.uwlax.edu/~shiwakot.prasann/clusterWebsite/server/api/footer.json') 
+  return $http.get('http://13.59.248.205/footer.json') 
             .success(function(data) { 
               return data; 
             })
